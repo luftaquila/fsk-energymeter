9d22280a65f0e69d066d9dca430de9d97303cd2e
오병준 <mail@luftaquila.io>
2025-06-07 02:11:49 +0900

    add: ssd1306 library

--- device/firmware/Core/Src/main.c
+++ device/firmware/Core/Src/main.c
@@ -26,7 +26,6 @@
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
-#include <stdio.h>
 #include "energymeter.h"
 /* USER CODE END Includes */
 
